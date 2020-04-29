d64c70fec7416aa3cfc19c48fd5b4eafa1b0cd9a
mce_yvieira@uoldiveo.com <mce_yvieira@uoldiveo.com>
2020-04-29 16:40:37 -0300

    VO

--- src/main/java/com/roadcard/app/controller/vo/UsuarioVO.java
+++ src/main/java/com/roadcard/app/controller/vo/UsuarioVO.java
@@ -1,4 +1,40 @@
 package com.roadcard.app.controller.vo;
 
+import com.roadcard.app.model.Usuario;
+
 public class UsuarioVO {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+
+    public UsuarioVO(Usuario usuario){
+        this.id = usuario.getId();
+        this.nome = usuario.getNome();
+        this.cpf = usuario.getCpf();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
