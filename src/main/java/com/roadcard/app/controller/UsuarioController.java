6cd3f400e8b5859b9ee118201f8ab2661ef4cbb0
mce_yvieira@uoldiveo.com <mce_yvieira@uoldiveo.com>
2020-04-29 16:41:25 -0300

    Criação dos métodos put, remove, post

--- src/main/java/com/roadcard/app/controller/UsuarioController.java
+++ src/main/java/com/roadcard/app/controller/UsuarioController.java
@@ -1,16 +1,17 @@
 package com.roadcard.app.controller;
 
+import com.roadcard.app.controller.form.UsuarioForm;
+import com.roadcard.app.controller.vo.UsuarioVO;
 import com.roadcard.app.model.Usuario;
 import com.roadcard.app.repository.UsuarioRepository;
 import com.roadcard.app.util.FormatUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuario")
@@ -47,5 +48,47 @@ public class UsuarioController {
         return null;
     }
 
-    
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioVO> atualizar(@PathVariable Long id,
+                                               @RequestBody @Valid UsuarioForm form){
+
+        Optional<Usuario> opt = usuarioRepository.findById(id);
+
+        if(opt.isPresent()){
+            Usuario usuario = form.atualizar(id, usuarioRepository);
+            return ResponseEntity.ok(new UsuarioVO(usuario));
+        }
+
+            return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity remover(@PathVariable Long id){
+
+        Optional<Usuario> opt = usuarioRepository.findById(id);
+        if(opt.isPresent()){
+            usuarioRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+            return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> cadastrar(@RequestBody UsuarioForm form){
+
+        if(validaForm(form)){
+            Usuario usuario = form.convert(form);
+            usuarioRepository.save(usuario);
+            return ResponseEntity.ok(usuario);
+        }
+            return ResponseEntity.badRequest().build();
+    }
+
+    private boolean validaForm(UsuarioForm form) {
+        Usuario usuario = usuarioRepository.findByCpfOrNome(form.getCpf(), form.getNome());
+        if(usuario != null){
+            return false;
+        }
+        return true;
+    }
 }
