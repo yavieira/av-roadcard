f766c2401cfaad60fb4fa095dc1b321af4a1fec2
mce_yvieira@uoldiveo.com <mce_yvieira@uoldiveo.com>
2020-04-29 15:21:47 -0300

    Criação do repository

--- src/main/java/com/roadcard/app/repository/UsuarioRepository.java
+++ src/main/java/com/roadcard/app/repository/UsuarioRepository.java
@@ -1,4 +1,11 @@
 package com.roadcard.app.repository;
 
-public interface UsuarioRepository {
+import com.roadcard.app.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Usuario findByCpfOrNome(String cpf, String nome);
 }
