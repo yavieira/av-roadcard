792e76c4f6d7f316cec861e4646b102187e45f3f
mce_yvieira@uoldiveo.com <mce_yvieira@uoldiveo.com>
2020-04-29 15:23:26 -0300

    Criação da classe de formatação

--- src/main/java/com/roadcard/app/util/FormatUtil.java
+++ src/main/java/com/roadcard/app/util/FormatUtil.java
@@ -1,4 +1,10 @@
 package com.roadcard.app.util;
 
 public class FormatUtil {
+    public static boolean validaCpf(String cpf) {
+        if(Character.isDigit(cpf.charAt(0))){
+            return true;
+        }
+        return false;
+    }
 }
